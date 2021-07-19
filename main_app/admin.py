a3773e15e344bf2bd76714a5bb2770f89420fc70
sunsetdee <andiilii@yahoo.com>
2021-07-19 13:52:14 -0700

    work on add rating

--- main_app/admin.py
+++ main_app/admin.py
@@ -6,4 +6,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Comment)
+admin.site.register(Rating)
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('events/create/', views.EventCreate.as_view(), name='events_create'),
     path('events/<int:pk>/update/', views.EventUpdate.as_view(), name='events_update'),
     path('events/<int:pk>/delete/', views.EventDelete.as_view(), name='events_delete'),
+    path('events/<int:event_id>/add_rating/', views.add_rating, name='add_rating'),
+
     path('accounts/signup/', views.signup, name='signup'),
     # path('events/<int:event_id>/add_photo', views.add_photo, name='add_photo'),
 ]
